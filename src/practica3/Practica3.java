616cbe18047453a6a75804740a7bdc9cf5386f20
usuario <usuario@10.22.143.34>
2015-10-14 10:06:35 +0100

    version 3

--- src/practica3/Practica3.java
+++ src/practica3/Practica3.java
@@ -1,22 +1,26 @@
 package practica3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Practica3 {
 
     public static void main(String[] args) {
         Histogram<String> histogram = new Histogram<>();
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
         
+        String pathName = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Practica3\\emailsfilev1.txt";
+        
+        try{
+            BufferedReader fileIn = new BufferedReader(new FileReader(pathName));
+            String mail;
+            while ((mail=fileIn.readLine()) != null){
+                if (mail.contains("@")){
+                    histogram.increment(mail.split("@")[1]);
+                }
+            }
+        }catch (Exception e){
+            System.out.println("Fichero no existe " + e);
+        }
         
         HistogramDisplay histo = new HistogramDisplay(histogram);
         histo.execute();
