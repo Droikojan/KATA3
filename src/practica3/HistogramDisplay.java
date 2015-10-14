34da0aa6b73946bac9b247b7a343edb6c0264133
usuario <usuario@10.22.143.34>
2015-10-14 09:21:43 +0100

    version 1

---
+++ src/practica3/HistogramDisplay.java
@@ -0,0 +1,44 @@
+package practica3;
+
+import java.awt.Dimension;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+
+public class HistogramDisplay extends ApplicationFrame{
+
+    public HistogramDisplay() {
+        super("HISTOGRAM");
+        setContentPane(createPanel());
+        pack();
+    }
+    
+    private ChartPanel createPanel(){
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
+        chartPanel.setPreferredSize(new Dimension (500, 400));
+        return chartPanel;
+    }
+    
+    private JFreeChart createChart(DefaultCategoryDataset dataset){
+        JFreeChart chart = ChartFactory.createBarChart(null, "Dominios", "Nº Emails", dataset, PlotOrientation.VERTICAL, true, false, false);
+        return chart;
+    }
+    
+    private DefaultCategoryDataset createDataset (){
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        dataset.addValue(10, "ENE", "gmail.com");
+        dataset.addValue(7, "ENE", "ulpgc.es");
+        dataset.addValue(5, "ENE", "hotmail.com");
+        dataset.addValue(18, "FEB", "gmail.com");
+        dataset.addValue(17, "FEB", "ulpgc.es");
+        dataset.addValue(15, "FEB", "hotmail.com");
+        return dataset;
+    }
+    
+    public void execute(){
+        setVisible(true);
+    }
+}
